27857e47e542abfa9bc84c1247d4f0615cabfff8
Kyle Kohnen <77641199+kylekohnen@users.noreply.github.com>
2023-04-12 18:45:30 -0700

    HTTP Client Initialization (#2)
    * created directory structure for custom http client
    
    * created base dunder methods for HttpClient class
    
    * created base request methods
    
    * created get request method

---
+++ nhl_data/api/http_client.py
@@ -0,0 +1,68 @@
+import logging
+from http import HTTPMethod
+
+import httpx
+
+logger = logging.getLogger(__name__)
+
+
+class HttpClient:
+    """
+    HTTP Client for connecting to the NHL API.
+    """
+
+    def __init__(self, base_url: str, raise_status_errors: bool = True) -> None:
+        self.base_url = base_url
+        self.raise_status_errors = raise_status_errors
+        self.client = httpx.Client(base_url=self.base_url, timeout=10)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.client.close()
+
+    def request(
+        self, method: HTTPMethod, endpoint: str, url_parameters: dict = None
+    ) -> dict | list:
+        """
+        Performs a HTTP Request to the specified endpoint.
+
+        :param method: the specific request type we want to send
+        :param endpoint: endpoint we want to send the request to
+        :param url_parameters: any additional parameters to add for the request,
+            defaults to None
+        :return: the JSON response from the request
+        """
+        response = self.client.request(method, endpoint, params=url_parameters)
+        return self._handle_response(response)
+
+    def _handle_response(self, response: httpx.Response) -> dict | list:
+        if self.raise_status_errors:
+            if response.status_code >= 500:
+                raise HttpServerError(response.status_code)
+            elif response.status_code >= 400:
+                raise HttpClientError(response.status_code)
+        return response.json()
+
+    def get(self, endpoint: str, url_parameters: dict = None) -> dict | list:
+        """
+        Performs a GET Request to the specified endpoint.
+
+        :param method: the specific request type we want to send
+        :param endpoint: endpoint we want to send the request to
+        :param url_parameters: any additional parameters to add for the request,
+            defaults to None
+        :return: the JSON response from the request
+        """
+        return self.request(
+            HTTPMethod.GET, endpoint=endpoint, url_parameters=url_parameters
+        )
+
+
+class HttpClientError(Exception):
+    pass
+
+
+class HttpServerError(Exception):
+    pass
---
+++ tests/api/test_http_client.py
@@ -0,0 +1,51 @@
+from unittest.mock import Mock
+
+import httpx
+import pytest
+
+from nhl_data.api.http_client import HttpClient, HttpClientError, HttpServerError
+
+TEST_URL = "https://testing.com"
+
+
+def test_request():
+    expected = {"random_key": "random_value"}
+    mock_response = Mock()
+    mock_response.request.return_value = httpx.Response(
+        status_code=200, json={"random_key": "random_value"}
+    )
+    with HttpClient(TEST_URL) as c:
+        c.client = mock_response
+        result = c.request("GET", "/")
+    assert result == expected
+
+
+def test_get():
+    expected = {"random": "value"}
+    mock_response = Mock()
+    mock_response.request.return_value = httpx.Response(
+        status_code=200, json={"random": "value"}
+    )
+    with HttpClient(TEST_URL) as c:
+        c.client = mock_response
+        result = c.get("/")
+    assert result == expected
+
+
+@pytest.mark.parametrize(("status"), [400, 500])
+def test_status_error_raise_exception(status):
+    error = HttpServerError if status >= 500 else HttpClientError
+    data = {"should": "error"}
+    response = Mock(status_code=status, json=data)
+    with pytest.raises(error, match=f"{status}"):
+        HttpClient(TEST_URL)._handle_response(response)
+
+
+@pytest.mark.parametrize(("status"), [400, 500])
+def test_status_error_dont_raise_exception(status):
+    data = {"show_up": "even_on_error"}
+    response = Mock(status_code=status)
+    response.json.return_value = data
+    with HttpClient(TEST_URL, raise_status_errors=False) as c:
+        val = c._handle_response(response)
+    assert val == data
