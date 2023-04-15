074e42a75cec6041b5627a60b2ebe6dabc0ae761
Kyle Kohnen <77641199+kylekohnen@users.noreply.github.com>
2023-04-15 16:23:25 -0700

    created base model

---
+++ nhl_data/models/base.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+
+class Model(ABC):
+    """
+    Base class that all Models from the NHL API are based off of.
+    """
+
+    @abstractmethod
+    def from_json(cls, data: dict):  # pragma: no cover
+        """
+        Helper function which removes specific keywords / fields from the
+        response data depending on the Model, and incorporates other models
+        when applicable.
+        It preserves the dataclass' `__init__` method, making initialization
+        easier for all subclasses.
+
+        This should be called rather than the model's `__init__` method.
+        This is because this method will account for possible data fields that may be
+        included from some response data, that is not accounted for in models.
+        Additionally, it replaces all camelCase fields to snake_case.
+
+        :param data: dictionary containing all the data (e.g. the response data)
+        :return: an instance of the model
+        """
+        raise NotImplementedError
