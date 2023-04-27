d2d03ca76bb106cc90afe8330455fc7a6e09420e
Kyle Kohnen <77641199+kylekohnen@users.noreply.github.com>
2023-04-26 20:40:06 -0700

    created plays model

--- nhl_data/models/game.py
+++ nhl_data/models/game.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 
 from nhl_data.models.base import Model
@@ -24,19 +26,24 @@ class Game(Model):
     home_team: Team = None
     players: dict = None
     venue: dict = None
-    all_plays: list = None
+    all_plays: list[Play] = None
     scoring_plays: list = None
     penalty_plays: list = None
     plays_by_period: list = None
-    current_play: dict = None
+    current_play: Play = None
     boxscore: dict = None
     decisions: dict = None
 
     @classmethod
     def from_response(cls, request_data: dict):
         data = convert_keys_to_snake_case(request_data)
-        game_data, live_data = data.get("game_data", dict()), data.get(
-            "live_data", dict()
+        game_data = data.get("game_data", dict())
+        live_data = data.get("live_data", dict())
+        plays_data = live_data.get("plays", dict())
+        all_plays = (
+            [Play.from_response(d) for d in plays_data.get("all_plays", [])]
+            if "all_plays" in plays_data
+            else None
         )
         return cls(
             pk=game_data.get("game", dict()).get("pk"),
@@ -58,11 +65,69 @@ class Game(Model):
             else None,
             players=game_data.get("players"),
             venue=game_data.get("venue"),
-            all_plays=live_data.get("plays", dict()).get("all_plays"),
-            scoring_plays=live_data.get("plays", dict()).get("scoring_plays"),
-            penalty_plays=live_data.get("plays", dict()).get("penalty_plays"),
-            plays_by_period=live_data.get("plays", dict()).get("plays_by_period"),
-            current_play=live_data.get("plays", dict()).get("current_play"),
+            all_plays=all_plays,
+            scoring_plays=plays_data.get("scoring_plays"),
+            penalty_plays=plays_data.get("penalty_plays"),
+            plays_by_period=plays_data.get("plays_by_period"),
+            current_play=Play.from_response(plays_data["current_play"])
+            if "current_play" in plays_data
+            else None,
             boxscore=live_data.get("boxscore"),
             decisions=live_data.get("decisions"),
+        )
+
+
+@dataclass
+class Play(Model):
+    """
+    Represents and contains all data for a single play from an NHL game.
+    """
+
+    players: list = None
+    event: str = None
+    event_type_id: str = None
+    description: str = None
+    secondary_type: str = None
+    strength_name: str = None
+    game_winning_goal: bool = None
+    empty_net: bool = None
+    penalty_severity: str = None
+    penalty_minutes: str = None
+    period: int = None
+    period_type: str = None
+    ordinal_num: str = None
+    period_time: str = None
+    period_time_remaining: str = None
+    date_time: str = None
+    goals_away: int = None
+    goals_home: int = None
+    coordinates: dict = None
+    team: Team = None
+
+    @classmethod
+    def from_response(cls, request_data: dict):
+        data = convert_keys_to_snake_case(request_data)
+        result_data = data.get("result", dict())
+        about_data = data.get("about", dict())
+        return cls(
+            players=data.get("players"),
+            event=result_data.get("event"),
+            event_type_id=result_data.get("event_type_id"),
+            description=result_data.get("description"),
+            secondary_type=result_data.get("secondary_type"),
+            strength_name=result_data.get("strength", dict()).get("name"),
+            game_winning_goal=result_data.get("game_winning_goal"),
+            empty_net=result_data.get("empty_net"),
+            penalty_severity=result_data.get("penalty_severity"),
+            penalty_minutes=result_data.get("penalty_minutes"),
+            period=about_data.get("period"),
+            period_type=about_data.get("period_type"),
+            ordinal_num=about_data.get("ordinal_num"),
+            period_time=about_data.get("period_time"),
+            period_time_remaining=about_data.get("period_time_remaining"),
+            date_time=about_data.get("date_time"),
+            goals_away=about_data.get("goals", dict()).get("away"),
+            goals_home=about_data.get("goals", dict()).get("home"),
+            coordinates=data.get("coordinates"),
+            team=Team.from_response(data["team"]) if "team" in data else None,
         )
---
+++ tests/models/test_game.py
@@ -0,0 +1,53 @@
+import pytest
+
+from nhl_data.models.game import Game, Play
+from nhl_data.models.team import Team
+
+game_test_cases = {
+    "short_game": (
+        {"gameData": {"game": {"pk": 2000020001}}},
+        Game(pk=2000020001),
+    ),
+    "empty_data": (dict(), Game()),
+    "team_within_game": (
+        {"gameData": {"teams": {"away": {"id": 1}}}},
+        Game(away_team=Team(id=1)),
+    ),
+    "play_within_game": (
+        {"liveData": {"plays": {"allPlays": [{"result": {"event": "Start"}}]}}},
+        Game(all_plays=[Play(event="Start")]),
+    ),
+}
+
+
+@pytest.mark.parametrize(
+    ("test_data", "expected"), game_test_cases.values(), ids=game_test_cases.keys()
+)
+def test_game_from_response(test_data, expected):
+    result = Game.from_response(test_data)
+    assert result == expected
+
+
+play_test_cases = {
+    "short_play": (
+        {"result": {"description": "Game"}},
+        Play(description="Game"),
+    ),
+    "empty_data": (dict(), Play()),
+    "penalty_play": (
+        {"result": {"penaltySeverity": "Minor", "penaltyMinutes": 2}},
+        Play(penalty_severity="Minor", penalty_minutes=2),
+    ),
+    "scoring_play": (
+        {"result": {"gameWinningGoal": False, "emptyNet": False}},
+        Play(game_winning_goal=False, empty_net=False),
+    ),
+}
+
+
+@pytest.mark.parametrize(
+    ("test_data", "expected"), play_test_cases.values(), ids=play_test_cases.keys()
+)
+def test_play_from_response(test_data, expected):
+    result = Play.from_response(test_data)
+    assert result == expected
