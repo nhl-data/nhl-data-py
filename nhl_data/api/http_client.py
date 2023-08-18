c1cfea5ee81ff415cd413e6caecc3c9966d7d0c7
Kyle Kohnen <77641199+kylekohnen@users.noreply.github.com>
2023-08-17 20:31:26 -0700

    Async HTTP Client (#15)
    * update dev container extensions
    
    * develop start of async client
    
    * create async request methods
    
    * update code based on new tests
    
    - ensure that code can be called asynchronously
    - add pytest-asyncio as a dependency
    
    * move pytest-asyncio to dev dependency

--- nhl_data/api/http_client.py
+++ nhl_data/api/http_client.py
@@ -1,7 +1,7 @@
 import logging
 from http import HTTPMethod
 
-from httpx import Client, Response
+from httpx import AsyncClient, Client, Response
 
 logger = logging.getLogger(__name__)
 
@@ -56,6 +56,60 @@ class HttpClient:
         :return: the JSON response from the request
         """
         return self.request(
+            HTTPMethod.GET, endpoint=endpoint, url_parameters=url_parameters
+        )
+
+
+class HttpClientAsync:
+    """
+    Asynchronous HTTP Client for connecting to the NHL API.
+    """
+
+    def __init__(self, base_url: str, raise_status_errors: bool = True) -> None:
+        self.base_url = base_url
+        self.raise_status_errors = raise_status_errors
+        self.client = AsyncClient(base_url=self.base_url, timeout=10)
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, exc_tb):
+        await self.client.aclose()
+
+    async def request(
+        self, method: HTTPMethod, endpoint: str, url_parameters: dict = None
+    ) -> Response:
+        """
+        Performs a HTTP Request to the specified endpoint.
+
+        :param method: the specific request type we want to send
+        :param endpoint: endpoint we want to send the request to
+        :param url_parameters: any additional parameters to add for the request,
+            defaults to None
+        :return: the response object
+        """
+        response = await self.client.request(method, endpoint, params=url_parameters)
+        return await self._handle_response(response)
+
+    async def _handle_response(self, response: Response) -> dict | list:
+        if self.raise_status_errors:
+            if response.status_code >= 500:
+                raise HttpServerError(response.status_code)
+            elif response.status_code >= 400:
+                raise HttpClientError(response.status_code)
+        return response
+
+    async def get(self, endpoint: str, url_parameters: dict = None) -> Response:
+        """
+        Performs a GET Request to the specified endpoint.
+
+        :param method: the specific request type we want to send
+        :param endpoint: endpoint we want to send the request to
+        :param url_parameters: any additional parameters to add for the request,
+            defaults to None
+        :return: the JSON response from the request
+        """
+        return await self.request(
             HTTPMethod.GET, endpoint=endpoint, url_parameters=url_parameters
         )
 
--- tests/api/test_http_client.py
+++ tests/api/test_http_client.py
@@ -1,9 +1,15 @@
-from unittest.mock import Mock
+import asyncio
+from unittest.mock import AsyncMock, Mock
 
 import httpx
 import pytest
 
-from nhl_data.api.http_client import HttpClient, HttpClientError, HttpServerError
+from nhl_data.api.http_client import (
+    HttpClient,
+    HttpClientAsync,
+    HttpClientError,
+    HttpServerError,
+)
 
 TEST_URL = "https://testing.com"
 
@@ -21,6 +27,41 @@ def test_request():
     assert result.json() == expected
 
 
+@pytest.mark.asyncio
+async def test_async_request():
+    expected = {"random_key": "random_value"}
+    mock_client = AsyncMock()
+    mock_client.request.return_value = httpx.Response(
+        status_code=200, json={"random_key": "random_value"}
+    )
+    async with HttpClientAsync(TEST_URL) as c:
+        c.client = mock_client
+        result = await c.request("GET", "/")
+    assert result.status_code == 200
+    assert result.json() == expected
+
+
+@pytest.mark.asyncio
+async def test_async_request_timed():
+    """Ensure that all requests are not being made synchronously."""
+
+    async def mock_request(*args, **kwargs):
+        await asyncio.sleep(0.2)
+        return httpx.Response(status_code=200, json={"random_key": "random_value"})
+
+    expected = {"random_key": "random_value"}
+    mock_client = AsyncMock()
+    mock_client.request.side_effect = mock_request
+    async with HttpClientAsync(TEST_URL) as c:
+        c.client = mock_client
+        start_time = asyncio.get_event_loop().time()
+        results = await asyncio.gather(*[c.request("GET", "/") for _ in range(10)])
+        end_time = asyncio.get_event_loop().time()
+    assert end_time - start_time < 2
+    assert all(r.status_code == 200 for r in results)
+    assert all(r.json() == expected for r in results)
+
+
 def test_get():
     expected = {"random": "value"}
     mock_client = Mock()
@@ -30,6 +71,20 @@ def test_get():
     with HttpClient(TEST_URL) as c:
         c.client = mock_client
         result = c.get("/")
+    assert result.status_code == 200
+    assert result.json() == expected
+
+
+@pytest.mark.asyncio
+async def test_async_get():
+    expected = {"random_key": "random_value"}
+    mock_client = AsyncMock()
+    mock_client.request.return_value = httpx.Response(
+        status_code=200, json={"random_key": "random_value"}
+    )
+    async with HttpClientAsync(TEST_URL) as c:
+        c.client = mock_client
+        result = await c.get("/")
     assert result.status_code == 200
     assert result.json() == expected
 
